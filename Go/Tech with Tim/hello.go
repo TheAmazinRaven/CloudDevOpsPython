16ba0fa816da37521b6bd01d0f849caafc86aeae
TheAmazinRaven <raejdickerson@gmail.com>
2022-06-26 21:58:37 +0000

    Setting up the game.

--- Go/Tech with Tim/hello.go
+++ Go/Tech with Tim/hello.go
@@ -5,10 +5,12 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to the Quiz Game! \n What is your name?")
 
+	fmt.Printf("Please enter your name: \n")
+
 	var name string
 
 	fmt.Scan(&name)
 
-	fmt.Println(name)
+	fmt.Printf("Hello %v, I hope you are ready to play!", name)
 
 }
