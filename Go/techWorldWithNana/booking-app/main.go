9655620a538f23aa9f3a1eed6669ad35d38591e8
Raven Dickerson <raven.dickerson@banno.com>
2022-08-08 11:35:52 -0500

    Added user input.

--- Go/techWorldWithNana/booking-app/main.go
+++ Go/techWorldWithNana/booking-app/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
+	bookings := []string{}
 
 	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -15,12 +19,54 @@ func main() {
 
 	fmt.Println(conferenceName)
 
-	var userName string
-	var userTickets int
-	// Get name from user
-	fmt.Scan(userName)
+	for /* i := 0; i < 50; i++ */ {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
+		// Get name from user
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		if userTickets > remainingTickets {
+			fmt.Printf("We only have %v tickets left. So you can't book %v tickets.\n", remainingTickets, userTickets)
+			break
+		}
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		/*
+			fmt.Printf("The whole slice: %v\n", bookings)
+			fmt.Printf("The first value: %v\n", bookings[0])
+			fmt.Printf("Slice type: %T\n", bookings)
+			fmt.Printf("Slice length: %v\n", len(bookings))
+		*/
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("These are the first names of the bookings: %v\n", firstNames)
+
+		if remainingTickets == 0 {
+			// ending the program
+			fmt.Println("Our conference is booked up! Come back next year!")
+			break
+		}
+	}
 
 }
