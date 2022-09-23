bf6f7cbe85237a3bbe0a599d4a8e8e85a40ff5f9
Raven Dickerson <raven.dickerson@banno.com>
2022-09-23 15:13:58 -0500

    Learning functions.

--- Go/functions/main.go
+++ Go/functions/main.go
@@ -1,10 +1,30 @@
 package main
 
-import (
-	"fmt"
-)
+func main() {
 
-func main {
+	printHelloWorld("Raven", 9)
+	printHelloWorld("James", 4)
+	summer := mathGuy(7, 5)
+	println(summer)
+	println(mathGuy(8, 2))
+	human("Raven", 28)
+	coolHuman("Nathan", 12)
+}
 
-	
+func printHelloWorld(something string, thing int) {
+	println("Hello world! :)")
+	println(something, thing)
+}
+
+func mathGuy(x int, y int) int {
+	sum := x + y
+	return sum
+}
+
+func human(name string, age int) {
+	println(name, age)
+}
+
+func coolHuman(name string, age int) {
+	human(name, age)
 }
