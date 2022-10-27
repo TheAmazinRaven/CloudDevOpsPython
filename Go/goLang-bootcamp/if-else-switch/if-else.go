09436868b61f05bbc810599f924d89683e52dbf2
Raven <raejdickerson@gmail.com>
2022-10-27 12:29:10 -0500

    -

---
+++ Go/goLang-bootcamp/if-else-switch/if-else.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// if conditional
+	name := "Sara"
+
+	if name == "Sara" {
+		fmt.Println("Your name is", name, ".")
+	}
+
+	// else statements
+	name2 := "Kyle"
+
+	if name2 == "Kyle" {
+		fmt.Println("Your name is", name2, ".")
+	} else {
+		fmt.Println("Your name is not", name2, ".")
+	}
+
+	// else if
+
+}
