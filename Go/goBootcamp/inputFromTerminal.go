3306a19e4f5eecb03b7743b3351e08f3779d9838
Raven <raejdickerson@gmail.com>
2022-11-01 15:17:00 -0500

    Code is being really weird?

--- Go/goBootcamp/inputFromTerminal.go
+++ Go/goBootcamp/inputFromTerminal.go
@@ -7,4 +7,11 @@ import (
 
 func main() {
 	fmt.Printf("%#v\n", os.Args)
+
+	fmt.Println("Path:", os.Args[0])
+	fmt.Println("1st Argument:", os.Args[1])
+	fmt.Println("2nd Argument:", os.Args[2])
+	fmt.Println("3rd Argument:", os.Args[3])
+
+	fmt.Println("Number of items inside os.Args:", len(os.Args))
 }
